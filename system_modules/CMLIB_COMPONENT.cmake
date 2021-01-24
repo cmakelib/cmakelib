18b995f4f681d087c4ff4d96da7a923b1c8c09fb
Jan Kubalek <janmkubalek@gmail.com>
2021-01-24 19:25:53 +0100

    Component BSEDEF renamed to CMDEF

--- system_modules/CMLIB_COMPONENT.cmake
+++ system_modules/CMLIB_COMPONENT.cmake
@@ -19,7 +19,7 @@ SET(_CMLIB_COMPONENT_REPO_NAME_PREFIX "cmakelib-component-"
 	"Filename prefix for components"
 )
 
-SET(_CMLIB_COMPONENT_AVAILABLE_LIST basedef storage util
+SET(_CMLIB_COMPONENT_AVAILABLE_LIST cmdef storage util
 	CACHE INTERNAL
 	"List of available components."
 )
