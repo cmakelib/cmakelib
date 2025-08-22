abce96e86740edc1b555db611e550e83bfe627c6
Jan Kubalek <janmkubalek@gmail.com>
2025-08-22 10:53:13 +0200

    Add basic test for ALWAYS_SCRIPT_MODE, TYPO and shorten directory names

--- test/TEST_FRAMEWORK/execution_functions/test_run_and_check_output/warning_tests/warning_case_3_caller.cmake
+++ test/TEST_FRAMEWORK/execution_functions/test_run_and_check_output/warning_tests/warning_case_3_caller.cmake
@@ -9,5 +9,5 @@ FIND_PACKAGE(CMLIB REQUIRED)
 
 INCLUDE("${CMAKE_CURRENT_LIST_DIR}/../../../../TEST.cmake")
 
-TEST_RUN_AND_CHECK_OUTPUT("test_output_warning_should_fail_no_match"
+TEST_RUN_AND_CHECK_OUTPUT("should_fail_no_match"
 	WARNING_MESSAGE "Test warning message")
